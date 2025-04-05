24093e066eae668ac576bc0b0621c2e9358d8e9c
miguelsoffarelli <miguelsoffarelli11@gmail.com>
2025-04-04 22:27:20 -0300

    generated copy_static function for copying files into public directory

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
 from textnode import *
+import os
+import shutil
+
+
+def copy_static(src, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+
+    os.mkdir(destination)
+
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(destination, item)
+
+        if os.path.isfile(src_path):
+            print(f"Copying file: {src_path} to {dest_path}")
+            shutil.copy(src_path, dest_path)
+        elif os.path.isdir(src_path):
+            print(f"Creating directory: {dest_path}")
+            copy_static(src_path, dest_path)
+
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    static_dir = "static"
+    public_dir = "public"
+    copy_static(static_dir, public_dir)
+    print("Static files copied successfully!")
 
 main()
